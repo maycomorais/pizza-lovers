3d1916caad9f965acdc6699ef6c77e1a47aa48cb
Maycon Morais <maycomorais@gmail.com>
2022-06-27 23:47:42 -0300

    <featured> add Class-Validator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserrDto } from './dto/create-users-dto';
 import { UsersService } from './users.services';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserrDto) {
+    return this.usersService.create(createUserDto);
+  }
 }
--- src/users/users.services.ts
+++ src/users/users.services.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserrDto } from './dto/create-users-dto';
+import { User } from './entities/users.entities';
+import { uuid } from 'uuidv4';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: User[] = [];
+  getAll() {
+    return this.users;
+  }
+
+  create(createUserDto: CreateUserrDto): User {
+    const newUser: User = { id: uuid(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
